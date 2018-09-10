ace859ec2d4225a84c49d9eb95810854c78237bc
Karzan Botani <karzan@botani.nu>
2018-09-10 16:02:09 +0200

    Added dependencies: cors, compression and helmet.

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser'),
-      cookieParser = require('cookie-parser'),
-      morgan = require('morgan'),
-      Rest = require('./classes/rest.class'),
-      Login = require('./classes/login.class'),
-      Search = require('./classes/search.class'),
-      CookieSession = require('./classes/cookie-session.class');
+  app = express(),
+  cors = require('cors'),
+  compression = require('compression'),
+  helmet = require('helmet'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  morgan = require('morgan'),
+  Rest = require('./classes/rest.class'),
+  Login = require('./classes/login.class'),
+  Search = require('./classes/search.class'),
+  CookieSession = require('./classes/cookie-session.class');
 
-process.on('unhandledRejection', error => console.log('unhandledRejection', error));
+process.on('unhandledRejection', error =>
+  console.log('unhandledRejection', error)
+);
 
 // Environment variables
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
+app.use(compression());
+app.use(helmet());
 app.use(morgan('dev')); // Logger.
 app.use(bodyParser.json()); // Reads a form's input and stores it as a JavaScript object accessible through req.body.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,25 +33,25 @@ app.use(new CookieSession().middleware());
 new Login(app);
 new Search(app);
 
-app.use(Rest.start({
-  dbCredentials: {
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'blog',
-    multipleStatements: true
-  },
-  baseUrl: '/rest',
-  idMap: {
-    posts_categories: 'post_id',
-    posts_comments: 'post_id',
-    users_comments: 'user_id'
-  },
-  runtimeErrors: false
-}));
+app.use(
+  Rest.start({
+    dbCredentials: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'blog',
+      multipleStatements: true
+    },
+    baseUrl: '/api',
+    idMap: {
+      posts_categories: 'post_id',
+      posts_comments: 'post_id',
+      users_comments: 'user_id'
+    },
+    runtimeErrors: false
+  })
+);
 
 global.dbQuery = Rest.query;
-
-app.use(express.static('./public'));
 
 app.listen(port, () => console.log(`Server listening on port ${port}.`));
